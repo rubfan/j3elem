f330ee5f6e9cb59d69943667064e4dfc7cf53db6
PetroBonuss <petro.pidlisnyi@gmail.com>
2018-08-29 15:10:47 +0300

    Second try to push Lesson4 tasks

---
+++ homework/lesson4/petr.podlesny/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"HWLesson4","l":"HWLesson4()"},{"p":"<Unnamed>","c":"HWLesson4","l":"main(String[])","url":"main-java.lang.String:A-"}]
---
+++ homework/lesson4/petr.podlesny/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"HWLesson4"}]
