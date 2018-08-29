28513c044eb155586fe3e1629ea6ca66b70e928c
romanborysenk <ramzes198725@gmail.com>
2018-08-29 15:42:59 +0300

    Lesson5

--- homework/lesson_5/Roman.Borysenko/Calculator.java
+++ homework/lesson_5/Roman.Borysenko/Calculator.java
@@ -7,64 +7,77 @@ public class Calculator {
         double firstOperand;
         double secondOperand;
         String operation;
-        int type;
+        int type =0;
 
-        System.out.println("1: Static calculator");
-        System.out.println("2: Calculator from keyboard");
-        System.out.println("3: Calculator with parameters from command line");
-        type = new Scanner(System.in).nextInt();
-        switch(type) {
+        while(type != 4) {
+            System.out.println("1: Static calculator");
+            System.out.println("2: Calculator from keyboard");
+            System.out.println("3: Calculator with parameters from command line");
+            System.out.println("4: Exit");
+            type = new Scanner(System.in).nextInt();
+            switch (type) {
 
-            case 1:
-                firstOperand = -15;
-                secondOperand=8;
-                System.out.println("First parameter:" + firstOperand + " SecondOperand: " + secondOperand);
-                System.out.println("Multiplication: " + getValueOfOperation("multiplication",
-                        firstOperand,secondOperand));
-                System.out.println("Division: " +
-                        getValueOfOperation("division",firstOperand,secondOperand));
-                System.out.println("Additioanal: " +
-                        getValueOfOperation("additioanal",firstOperand,secondOperand));
-                System.out.println("Subtraction: " +
-                        getValueOfOperation("subtraction",firstOperand,secondOperand));
-                System.out.println("Remaind Of  The Division: " +
-                        getValueOfOperation("remaindofdivision",firstOperand,secondOperand));
-                System.out.println("Module:  First Parameter " + getModule(firstOperand) +
-                        " Second Parameter: " + getModule(secondOperand));
-                break;
+                case 1:
+                    firstOperand = -15;
+                    secondOperand = 8;
+                    System.out.println("First parameter:" + firstOperand + " SecondOperand: " + secondOperand);
+                    System.out.println("Multiplication: " + getValueOfOperation("multiplication",
+                            firstOperand, secondOperand));
+                    System.out.println("Division: " +
+                            getValueOfOperation("division", firstOperand, secondOperand));
+                    System.out.println("Additioanal: " +
+                            getValueOfOperation("additioanal", firstOperand, secondOperand));
+                    System.out.println("Subtraction: " +
+                            getValueOfOperation("subtraction", firstOperand, secondOperand));
+                    System.out.println("Remaind Of  The Division: " +
+                            getValueOfOperation("remaindofdivision", firstOperand, secondOperand));
+                    System.out.println("Module:  First Parameter " + getModule(firstOperand) +
+                            " Second Parameter: " + getModule(secondOperand));
+                    System.out.println();
+                    break;
 
-            case 2:
-                System.out.println("Please Enter operation (division, multiplication, "+"" +
-                        "additioanal, subtraction remaindofdivision, module): ");
-                operation = new Scanner(System.in).next();
-                System.out.println("Please Enter first parameter:");
-                firstOperand = new Scanner(System.in).nextDouble();
-                if (operation.equals("module")) {
-                    System.out.println("Module: " + getModule(firstOperand));
-                }
-                else {
-                    System.out.println("Please Enter second parameter:");
-                    secondOperand = new Scanner(System.in).nextDouble();
-                    System.out.println(operation + ": " +
-                            getValueOfOperation(operation, firstOperand, secondOperand));
-                }
-                break;
+                case 2:
+                    System.out.println("Please Enter operation (division, multiplication, " + "" +
+                            "additioanal, subtraction remaindofdivision, module): ");
+                    operation = new Scanner(System.in).next();
+                    System.out.println("Please Enter first parameter:");
+                    firstOperand = new Scanner(System.in).nextDouble();
+                    if (operation.equals("module")) {
+                        System.out.println("Module: " + getModule(firstOperand));
+                    } else {
+                        System.out.println("Please Enter second parameter:");
+                        secondOperand = new Scanner(System.in).nextDouble();
+                        if(getValueOfOperation(operation, firstOperand, secondOperand) == null) {
+                            System.out.println("Wrong operation!!!!");
+                            System.out.println("Please Enter operation (division, multiplication, " + "" +
+                                    "additioanal, subtraction remaindofdivision, module): ");
+                            operation = new Scanner(System.in).next();
+                        }
+                        System.out.println(operation + ": " +
+                                getValueOfOperation(operation, firstOperand, secondOperand));
+                    }
+                    System.out.println();
+                    break;
 
-            case 3:
-                operation = args[0];
-                firstOperand =Double.parseDouble(args[1]);
-                secondOperand = Double.parseDouble(args[2]);
-                System.out.println("First parameter:" + firstOperand +
-                        " Second parameter: " + secondOperand);
-                if (operation.equals("module")) {
-                    System.out.println("Module: " + getModule(firstOperand)  + getModule(secondOperand));
-                }
-                else {
-                    System.out.println(operation + ": " +
-                            getValueOfOperation(operation, firstOperand, secondOperand));
-                }
-                break;
+                case 3:
+                    operation = args[0];
+                    firstOperand = Double.parseDouble(args[1]);
+                    secondOperand = Double.parseDouble(args[2]);
+                    System.out.println("First parameter:" + firstOperand +
+                            " Second parameter: " + secondOperand);
+                    if (operation.equals("module")) {
+                        System.out.println("Module: " + getModule(firstOperand) + getModule(secondOperand));
+                    } else {
+                        System.out.println(operation + ": " +
+                                getValueOfOperation(operation, firstOperand, secondOperand));
+                    }
+                    System.out.println();
+                    break;
 
+                case 4:
+                    return;
+
+            }
         }
     }
 
