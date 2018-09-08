3defbc88eda263f8ee9a16af8c6c5708c5d1f3d2
AleksandrPashkevich <o632681383@gmail.com>
2018-09-08 09:55:17 +0300

    triangle works but not pretty

--- homework/lesson_7/AleksandrPashkevich/src/ua/hillel/odesa/je3/Main.java
+++ homework/lesson_7/AleksandrPashkevich/src/ua/hillel/odesa/je3/Main.java
@@ -28,28 +28,45 @@ public class Main {
 
     private static void paskalTriangle(int val) {
 
-    int [] [] arr = new int [val+ 1] [val + 1];
+        int[][] arr = new int[val + 1][val + 1];
 
-    for (i = 0; i < arr.length; i++){
+        for (int i = 1; i < arr.length; i++) {
 
-    	for (int j = 0; j < arr[i].length; j++){
-    		if (j>0){
+            int temp = arr.length - i;
 
-    		arr[i][j] = arr[i-1][j-1] + arr [i-1][j]
-    	} else { 
-    		arr[i][j] = 1;
-    	}
+            paneShow(temp + 1);
 
-    	System.out.print(arr[i][j] + " ");
-    	
-    	}
+            for (int j = 0; j < i; j++) {
 
-    	System.out.println();
+                if (j > 0) {
+
+                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+
+                } else {
+
+                    arr[i][j] = 1;
+                }
+
+                System.out.print(arr[i][j] + " ");
+
+            }
+
+            System.out.println();
+        }
     }
+
+
+    private static void paneShow(int i) {
+
+        for (int l = i; l > 0; --l) {
+
+            System.out.print(" ");
+        }
     }
 
     private static void errorShow() {
 
         System.out.println("ERROR, check input data.");
+        
     }
 }
