551ea5cff2b91113195ea2c7c80889feb3bbf19b
AleksandrPashkevich <o632681383@gmail.com>
2018-09-20 23:56:19 +0300

    lesson 8, sorry without tests

--- homework/lesson_8/AleksandrPashkevich/src/main/java/Task3.java
+++ homework/lesson_8/AleksandrPashkevich/src/main/java/Task3.java
@@ -2,6 +2,19 @@ public class Task3 {
 
     public static void main(String[] args) {
 
+//(n * (n-1) * (n-2)/3!
+        AlgorithmExtend alg = new AlgorithmExtend();
+
+        int n = (int) (Math.random() * 10 + 3);
+        int calc = n*(n-1) *(n - 2)/6;
+        int k = alg.algCheck(n);
+        System.out.println("n = " + n + ", ((n)(n-1)(n-2)/3! = " + calc);
+        System.out.println("Actual value: " + k);
+
+        if (calc == k){
+
+            System.out.println("formula looks working!");
+        }
 
 
     }
